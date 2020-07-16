8e47bc17a2265a427059ff1df91d45c75add3843
Senst <senstb@a483e7a02db8.ant.amazon.com>
2020-07-16 08:08:20 -0700

    Reset ALZ Config

---
+++ aws-landing-zone-configuration/add-on/newAccountNotificationAddOn/checkAVMStabilization.py
@@ -0,0 +1,42 @@
+import json
+import boto3
+import time
+
+def lambda_handler(event, context):
+
+   return stabilizeStateMachine()
+
+def stabilizeStateMachine():
+    sfnStatus = checkStateMachineStatus()
+    waitCount = 0 
+    
+    #Check to see if Step Functions succeed; after 30 seconds fail out
+    while ((sfnStatus != 'SUCCEEDED') or (waitCount > 3)):
+        time.sleep(10)
+        sfnStatus = checkStateMachineStatus()
+        waitCount += 1
+    else:
+        if ((sfnStatus != 'SUCCEEDED') and (waitCount > 3)):
+            return 'Step Function Failed to Stabilize; check execution logs'
+        else:
+            return 'Step Function succeeded.'
+
+def checkStateMachineStatus():
+    # cfnClient = boto3.client('cloudformation', region_name='us-east-1')
+    sfnClient = boto3.client('stepfunctions', region_name='us-east-1')
+    
+    """
+    cfnResponseJson = cfnClient.list_stack_set_operations(
+        StackSetName='<application name>',
+        MaxResults='1'
+    )
+    """
+    
+    sfnResponseJson = sfnClient.list_executions(
+        stateMachineArn='arn:aws:states:us-east-1:${AccountID}:stateMachine:LandingZoneLaunchAVMStateMachine',
+        maxResults= 1
+    )
+    
+    latestExecutionStatus = sfnResponseJson['executions'][0]['status']
+        
+    return latestExecutionStatus
