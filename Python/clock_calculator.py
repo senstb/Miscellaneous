cc10524f566582a44a4556c2ea80d0d48c36db58
Braden <senstb@amazon.com>
2020-10-29 17:43:29 -0700

    Update with user input functions

--- Python/clock_calculator.py
+++ Python/clock_calculator.py
@@ -12,7 +12,6 @@ class TimeStamp(object):
     def add_minute(self, startminute=int, addminute=int):
         return_minute_object = [0,startminute]
         sum_minute = startminute + addminute
-        print(str(sum_minute))
         if sum_minute > 59:
             return_minute_object[1] = sum_minute - 60
             return_minute_object[0] = 1
@@ -46,33 +45,41 @@ def parse_args(args):
     parser = argparse.ArgumentParser(
         description="Takes in hour and minute and delta hour and minute."
     )
-    parser.add_argument("-ho","--hour",help="Start hour value: hh", type=int, default="00")
-    parser.add_argument("-mi","--minute",help="Start minute value: mm", type=int, default="00")
-    parser.add_argument("-ah","--addhour", help="Input hour value to add: hh", type=int, default="00")
-    parser.add_argument("-am","--addminute", help="Input minute value to add: mm", type=int, default="00")
+    parser.add_argument("-ho","--hour",help="Start hour value: hh", nargs='?')
+    parser.add_argument("-mi","--minute",help="Start minute value: mm",  nargs='?')
+    parser.add_argument("-ah","--addhour", help="Input hour value to add: hh", nargs='?')
+    parser.add_argument("-am","--addminute", help="Input minute value to add: mm", nargs='?')
     return parser.parse_args(args)
-
 
 
 def main():
     args = parse_args(sys.argv[1:])
-    args_dict = vars(args)
+    args_dict = {}
+    if None not in vars(args).viewvalues():
+        args_dict = vars(args)
+    else:
+        args_dict["hour"] = input("Starting hour: ")
+        args_dict["minute"] = input("Starting minute: ")
+        args_dict["addhour"] = input("Change in hour: ") 
+        args_dict["addminute"] = input("Change in minute: ")
+
 
     timestamp = TimeStamp(**args_dict)
 
-    print("startMin: " + str(args.minute))
-    print("startHr: " + str(args.hour))
-    print("addMin: " + str(args.addminute))
-    print("addHr: " + str(args.addhour))
-    minute = timestamp.add_minute(args.minute, args.addminute)
-    hour = timestamp.add_hour(args.hour, args.addhour, minute[0])
+    print("startMin: " + str(args_dict["minute"]))
+    print("startHr: " + str(args_dict["hour"]))
+    print("addMin: " + str(args_dict["addminute"]))
+    print("addHr: " + str(args_dict["addhour"]))
+
+    minute = timestamp.add_minute(args_dict["minute"], args_dict["addminute"])
+    hour = timestamp.add_hour(args_dict["hour"], args_dict["addhour"], minute[0])
 
     if len(str(hour)) <2:
         output_hour = "0" + str(hour)
     else:
         output_hour = str(hour)
-    if len(str(minute)) <2:
-        output_minute = "0" +str(minute[1])
+    if len(str(minute[1])) <2:
+        output_minute = "0" + str(minute[1])
     else:
         output_minute = str(minute[1])
 
